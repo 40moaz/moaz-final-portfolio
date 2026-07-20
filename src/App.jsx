230dc73c5e6393424f36ae1b1b36bff8e8cfaf82
Moaz Ali <amoaz14109@gmail.com>
2026-07-20 17:41:59 +0300

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 import { Toaster } from "react-hot-toast";
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
         </div>
@@ -15,19 +25,15 @@ const App = () => {
         <Tech />
         <Works />
         <Feedbacks />
-        <div className='relative z-0'>
+        <div className="relative z-0">
           <Contact />
           <StarsCanvas />
         </div>
-        
-        <Toaster
-position="top-right"
-reverseOrder={false}
-/>
+
+        <Toaster position="top-right" reverseOrder={false} />
       </div>
-      
     </BrowserRouter>
   );
-}
+};
 
 export default App;
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,26 +39,26 @@ const Contact = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!form.name.trim()) {
-  toast.error("Please enter your name.");
-  return;
-}
+      toast.error("Please enter your name.");
+      return;
+    }
 
-if (!form.email.trim()) {
-  toast.error("Please enter your email.");
-  return;
-}
+    if (!form.email.trim()) {
+      toast.error("Please enter your email.");
+      return;
+    }
 
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-if (!emailRegex.test(form.email)) {
-  toast.error("Please enter a valid email.");
-  return;
-}
+    if (!emailRegex.test(form.email)) {
+      toast.error("Please enter a valid email.");
+      return;
+    }
 
-if (!form.message.trim()) {
-  toast.error("Please write a message.");
-  return;
-}
+    if (!form.message.trim()) {
+      toast.error("Please write a message.");
+      return;
+    }
     setLoading(true);
 
     emailjs
@@ -72,14 +72,15 @@ if (!form.message.trim()) {
           to_email: "moaz.a.ismail1@gmail.com",
           message: form.message,
         },
-        "QHmHOZ2J-FPR__4te"
+        "QHmHOZ2J-FPR__4te",
       )
       .then(
         () => {
           setLoading(false);
-toast.success(
-  "Message sent successfully! I'll get back to you soon."
-);          setForm({
+          toast.success(
+            "Message sent successfully! I'll get back to you soon.",
+          );
+          setForm({
             name: "",
             email: "",
             message: "",
@@ -88,9 +89,9 @@ toast.success(
         (error) => {
           setLoading(false);
           console.error(error);
-        toast.error("Something went wrong. Please try again.");
-        }
-);
+          toast.error("Something went wrong. Please try again.");
+        },
+      );
   };
 
   return (
@@ -99,13 +100,14 @@ toast.success(
     >
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
-        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
       >
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
-<div className="grid  gap-2 mb-10">
-  <a href="mailto:moaz.a.ismail1@gmail.com"
-    className="
+        <div className="grid  gap-2 mb-10">
+          <a
+            href="mailto:moaz.a.ismail1@gmail.com"
+            className="
     flex items-center gap-4
     rounded-xl
     border border-white/10
@@ -117,14 +119,15 @@ toast.success(
     hover:border-[#915EFF]
     hover:bg-[#915EFF]/10
     hover:-translate-y-1
-    "  >
-    <FaEnvelope size={20} />
-    <span>moaz.a.ismail1@gmail.com</span>
-  </a>
+    "
+          >
+            <FaEnvelope size={20} />
+            <span>moaz.a.ismail1@gmail.com</span>
+          </a>
 
-  <a
-    href="tel:01113140942"
-    className="
+          <a
+            href="tel:01113140942"
+            className="
     flex items-center gap-4
     rounded-xl
     border border-white/10
@@ -136,14 +139,15 @@ toast.success(
     hover:border-[#915EFF]
     hover:bg-[#915EFF]/10
     hover:-translate-y-1
-    "  >
-    <FaPhoneAlt size={20} />
-    <span>01113140942</span>
-  </a>
+    "
+          >
+            <FaPhoneAlt size={20} />
+            <span>01113140942</span>
+          </a>
 
-  <a
-    href="tel:01039010884"
-    className="
+          <a
+            href="tel:01039010884"
+            className="
     flex items-center gap-4
     rounded-xl
     border border-white/10
@@ -155,16 +159,17 @@ toast.success(
     hover:border-[#915EFF]
     hover:bg-[#915EFF]/10
     hover:-translate-y-1
-    "  >
-    <FaPhoneAlt size={20} />
-    <span>01039010884</span>
-  </a>
+    "
+          >
+            <FaPhoneAlt size={20} />
+            <span>01039010884</span>
+          </a>
 
-  <a
-    href="https://www.linkedin.com/in/moaz-a-ismail1/"
-    target="_blank"
-    rel="noopener noreferrer"
-    className="
+          <a
+            href="https://www.linkedin.com/in/moaz-a-ismail1/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="
     flex items-center gap-4
     rounded-xl
     border border-white/10
@@ -176,16 +181,17 @@ toast.success(
     hover:border-[#915EFF]
     hover:bg-[#915EFF]/10
     hover:-translate-y-1
-    "  >
-    <FaLinkedin size={20} />
-    <span>LinkedIn</span>
-  </a>
+    "
+          >
+            <FaLinkedin size={20} />
+            <span>LinkedIn</span>
+          </a>
 
-  <a
-    href="https://www.facebook.com/moza.a.ismail/"
-    target="_blank"
-    rel="noopener noreferrer"
-    className="
+          <a
+            href="https://www.facebook.com/moza.a.ismail/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="
     flex items-center gap-4
     rounded-xl
     border border-white/10
@@ -197,95 +203,91 @@ toast.success(
     hover:border-[#915EFF]
     hover:bg-[#915EFF]/10
     hover:-translate-y-1
-    "  >
-    <FaFacebook size={20} />
-    <span>Facebook</span>
-  </a>
-</div>
+    "
+          >
+            <FaFacebook size={20} />
+            <span>Facebook</span>
+          </a>
+        </div>
         <form
           ref={formRef}
           onSubmit={handleSubmit}
-          className='mt-12 flex flex-col gap-8'
+          className="mt-12 flex flex-col gap-8"
         >
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Name</span>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name</span>
             <input
-              type='text'
+              type="text"
               required
-              name='name'
+              name="name"
               value={form.name}
               onChange={handleChange}
-placeholder="Enter your name"              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              placeholder="Enter your name"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
           </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your email</span>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your email</span>
             <input
-              type='email'
-              name='email'
+              type="email"
+              name="email"
               value={form.email}
               required
               onChange={handleChange}
-placeholder="Enter your email"              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              placeholder="Enter your email"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
           </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Message</span>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Message</span>
             <textarea
               rows={7}
-              name='message'
+              name="message"
               value={form.message}
               required
               onChange={handleChange}
-placeholder="Write your message..."              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              placeholder="Write your message..."
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
           </label>
 
           <div className="flex gap-4 flex-wrap">
+            <button
+              disabled={loading}
+              type="submit"
+              className="bg-[#915EFF] hover:bg-[#7a47ff] transition-all py-3 px-8 rounded-xl text-white font-bold flex items-center gap-3 disabled:opacity-70"
+            >
+              {loading ? (
+                <>
+                  <Oval
+                    height={20}
+                    width={20}
+                    color="#fff"
+                    secondaryColor="#ffffff55"
+                    strokeWidth={4}
+                  />
+                  Sending...
+                </>
+              ) : (
+                "Send Message"
+              )}
+            </button>
 
-<button
-  disabled={loading}
-  type="submit"
-  className="bg-[#915EFF] hover:bg-[#7a47ff] transition-all py-3 px-8 rounded-xl text-white font-bold flex items-center gap-3 disabled:opacity-70"
->
-
-{loading ? (
-<>
-<Oval
-height={20}
-width={20}
-color="#fff"
-secondaryColor="#ffffff55"
-strokeWidth={4}
-/>
-
-Sending...
-</>
-) : (
-"Send Message"
-)}
-
-</button>
-
-<a
-href="/Moaz_Ali_CV.pdf"
-download
-className="border border-[#915EFF] text-white hover:bg-[#915EFF] transition-all py-3 px-8 rounded-xl font-bold flex items-center gap-3"
->
-
-<FaDownload />
-
-Download CV
-
-</a>
-
-</div>
+            <a
+              href="/Moaz_Ali_CV.pdf"
+              download
+              className="border border-[#915EFF] text-white hover:bg-[#915EFF] transition-all py-3 px-8 rounded-xl font-bold flex items-center gap-3"
+            >
+              <FaDownload />
+              Download CV
+            </a>
+          </div>
         </form>
       </motion.div>
 
       <motion.div
         variants={slideIn("right", "tween", 0.2, 1)}
-        className='xl:flex-1 xl:h-auto md:h-[450px] h-[300px]'
+        className="xl:flex-1 xl:h-auto md:h-[450px] h-[300px]"
       >
         <EarthCanvas />
       </motion.div>
